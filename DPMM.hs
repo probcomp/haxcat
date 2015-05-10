95c867ddd07f34423e6f39aa95778a2cf89c8f97
Baxter Eaves <baxyjr@gmail.com>
2015-05-10 16:16:47 -0400

    Added get weights and probabilities

--- DPMM.hs
+++ DPMM.hs
@@ -4,6 +4,8 @@ import Data.Random.RVar
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe
+import Numeric.SpecFunctions (logGamma)
+
 
 type ClusterID = Int
 type DatumID = Int
@@ -17,6 +19,35 @@ data NIG = NIG { count :: Int
                , sumx :: Double
                , sumxsq :: Double
                }
+
+emptyNIG :: NIG
+emptyNIG = NIG 0 0 0 
+
+logp :: Double -> NIG -> Double
+logp datum NIG{..} = -0.5*log (2*pi) + niglognorm r'' nu'' s'' - niglognorm r' nu' s'
+    where r' = r + count'
+          nu' =  nu + count'
+          m' = (r*m + sumx)/r'
+          s' = s + sumxsq + r*m*m  - r'*m'*m'
+          r'' = r + count' + 1
+          nu'' =  nu + count' + 1
+          m'' = (r*m + sumx + datum)/r''
+          s'' = s + sumxsq + datum*datum + r*m*m  - r''*m''*m''
+          m = 0
+          r = 1
+          nu = 1
+          s = 1
+          count' = fromIntegral count
+
+getweights :: Double -> DPMM -> [(ClusterID, Double)]
+getweights datum DPMM{..} = new:existing
+    where new = (fst (M.findMax components) + 1, logp datum emptyNIG + 0)
+          existing = [(componentIdx, logp datum cmpnt + log (fromIntegral (count cmpnt)))
+                      | (componentIdx, cmpnt) <- M.toList components] 
+
+
+niglognorm :: Double -> Double -> Double -> Double
+niglognorm r nu s = 0.5*(nu+1) * log 2 + 0.5 * log pi - 0.5 *log r - 0.5*nu * log s + logGamma (nu/2)
 
 bogoinit :: [Double] -> DPMM
 bogoinit dataset = DPMM (M.singleton 0 component) assignment dataset'
