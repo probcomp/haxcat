e7e49c2f89c40d1727cabdd4628560d0759b6dd9
Baxter Eaves <baxyjr@gmail.com>
2015-05-10 14:01:44 -0400

    list of components to map

--- DPMM.hs
+++ DPMM.hs
@@ -1,8 +1,11 @@
 import Data.Random.RVar
+import Data.Map (Map)
+import qualified Data.Map as M
 
 type ClusterID = Int
+type DatumID = Int
 
-data DPMM = DPMM { components :: [NIG]
+data DPMM = DPMM { components :: Map ClusterID NIG
                  , assignment :: [ClusterID]
                  , dataset :: [Double]
                  }
@@ -13,7 +16,7 @@ data NIG = NIG { count :: Int
                }
 
 bogoinit :: [Double] -> DPMM
-bogoinit dataset = DPMM [component] assignment dataset
+bogoinit dataset = DPMM (M.singleton 0 component) assignment dataset
     where component = NIG (length dataset) (sum dataset) (sum $ map (\ x -> x*x) dataset)
           assignment = replicate (length dataset) 0
 
@@ -21,8 +24,8 @@ gibbsSweep :: DPMM -> RVar DPMM
 gibbsSweep = undefined 
 
 rmdatum :: NIG -> Double -> NIG
-rmdatum NIG{count=ct, sumx=sx, sumxsq=sx2} x = NIG (ct - 1) (sx - x) (sxq - x*x)
+rmdatum NIG{count=ct, sumx=sx, sumxsq=sx2} x = NIG (ct - 1) (sx - x) (sx2 - x*x)
 
 addDatum :: NIG -> Double -> NIG
-addDatum NIG{count=ct, sumx=sx, sumxsq=sx2} x = NIG (ct + 1) (sx + x) (sxq + x*x)
+addDatum NIG{count=ct, sumx=sx, sumxsq=sx2} x = NIG (ct + 1) (sx + x) (sx2 + x*x)
 
