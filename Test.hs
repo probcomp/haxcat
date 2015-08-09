0ce3f9868c6f81f3c4e4dedf5ff85b0409a51ee3
Alexey Radul <axch@mit.edu>
2015-08-09 16:51:30 -0400

    Rewrite my KL measurements in smaller pieces, in terms of the concept of tail-assessable distributions.

--- Test.hs
+++ Test.hs
@@ -12,7 +12,7 @@ import DPMM
 
 type LogDensity a = a -> Double
 type Assessable a = (RVar a, LogDensity a)
-
+type TailAssessable a = RVar (Assessable a)
 
 mixture_density :: [LogDensity a] -> LogDensity a
 mixture_density ds x = logsumexp (map ($ x) ds) - log (fromIntegral $ length ds)
@@ -28,20 +28,47 @@ two_modes = (sample, logd) where
           normal (-3) 1
     logd x = logsumexp [logPdf (Normal 3 1) x, logPdf (Normal (-3) 1) x] - log 2
 
+two_modes_ta :: TailAssessable Double
+two_modes_ta = do
+  pos_mode <- bernoulli (0.5 :: Double)
+  if pos_mode then
+      return $ gaussian 3 1
+  else
+      return $ gaussian (-3) 1
+
+gaussian :: Double -> Double -> Assessable Double
+gaussian mu sig = (normal mu sig, logPdf (Normal mu sig))
+
 estimate_KL :: Assessable a -> LogDensity a -> Int -> RVar Double
 estimate_KL from to sample_ct = do
   input <- replicateM sample_ct (fst from)
   return $ (sum $ map term input) / (fromIntegral $ length input)
     where term x = (snd from) x - to x
 
-measure_kl :: Assessable Double -> Int -> Int -> Int -> Int -> RVar Double
-measure_kl data_gen train_data_ct iter_ct chain_ct test_ct = do
+approximately_assess :: Int -> TailAssessable a -> RVar (LogDensity a)
+approximately_assess ct dist = do
+  assessors <- liftM (map snd) $ replicateM ct dist
+  return $ mixture_density assessors
+
+estimate_KL_ta :: Assessable a -> TailAssessable a -> Int -> Int -> RVar Double
+estimate_KL_ta from to latents_ct sample_ct = do
+  density <- approximately_assess latents_ct to
+  estimate_KL from density sample_ct
+
+-- sampleIO $ estimate_KL_ta two_modes two_modes_ta 1000 2000 is ~5e-5
+
+dpmm_dist :: DPMM -> Assessable Double
+dpmm_dist dpmm = (return $ error "What?", predictive_logdensity dpmm)
+
+trained_dpmm :: [Double] -> Int -> TailAssessable Double
+trained_dpmm input iters = liftM dpmm_dist $ train_dpmm input iters
+
+measure_dpmm_kl :: Assessable Double -> Int -> Int -> Int -> Int -> RVar Double
+measure_dpmm_kl data_gen train_data_ct iter_ct chain_ct test_ct = do
   input <- replicateM train_data_ct (fst data_gen)
-  results <- replicateM chain_ct $ train_dpmm input iter_ct
-  let result_density = mixture_density (map predictive_logdensity results)
-  estimate_KL data_gen result_density test_ct
+  estimate_KL_ta data_gen (trained_dpmm input iter_ct) chain_ct test_ct
 
 sampleIO :: RVar a -> IO a
 sampleIO = sampleRVar
 
--- e.g. sampleIO $ measure_kl two_modes 1000 20 10 500
+-- e.g. sampleIO $ measure_dpmm_kl two_modes 1000 20 10 500
