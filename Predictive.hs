395123722c2a06343f68a828320028a566ef17bc
Alexey Radul <axch@mit.edu>
2015-09-27 13:50:20 -0400

    Draft the predictive PDF for a crosscat state.

--- Predictive.hs
+++ Predictive.hs
@@ -10,8 +10,11 @@ import Data.Maybe (fromJust)
 import Data.Random.RVar
 import Data.Traversable
 
+import qualified Numeric.Log as Log (sum)
+
 import Models
 import Types
+import RowSweep (view_weights)
 
 view_cluster_sample :: View -> RVar ClusterID
 view_cluster_sample View{..} = sample_predictive view_counts view_crp
@@ -28,4 +31,15 @@ view_sample v@View{..} = do
 cc_sample :: Crosscat -> RVar Row
 cc_sample Crosscat{..} = liftM (M.foldl' M.union M.empty) per_view where
     per_view = mapM view_sample cc_views
-                            
+
+view_pdf_predictive :: View -> PDF Row
+view_pdf_predictive view row = Log.sum $ map snd $ view_weights view row
+
+-- Treats missing columns correctly, namely by ignoring them.
+cc_pdf_predictive :: Crosscat -> PDF Row
+cc_pdf_predictive Crosscat{..} row =
+    -- TODO Perhaps this would be faster if I used the cc_partition to
+    -- per-filter the data in the row, but this remains correct
+    -- because view_predictive ignores extra columns (which maybe it
+    -- shouldn't?)
+    product $ map (flip view_pdf_predictive row) $ M.elems cc_views
