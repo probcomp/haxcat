189d746fcad15b1a7665c2474eafcdc56ada5c2e
Taylor R Campbell <campbell+haxcat@mumble.net>
2015-09-27 17:52:21 +0000

    Add bernoulli_weight to Utils.hs.
    This calculates log (a/(a + b)) using log1p to avoid losing precision
    when a >>> b.

--- Utils.hs
+++ Utils.hs
@@ -4,7 +4,7 @@ import Data.Random.RVar
 import Data.Random.Distribution.Categorical (weightedCategorical)
 
 import Numeric.Log hiding (sum)
-import Numeric.SpecFunctions (logBeta, logFactorial)
+import Numeric.SpecFunctions (log1p, logBeta, logFactorial)
 
 flipweights :: [(a, Double)] -> RVar a
 flipweights weights = weightedCategorical [(exp (p - maxp), idx) | (idx, p) <- weights]
@@ -27,3 +27,21 @@ log_domain = Exp . log
 
 beta :: Double -> Double -> Log Double
 beta a b = Exp $ logBeta a b
+
+-- bernoulli_weight alpha beta
+--
+--      Log-domain weight of a Bernoulli success given nonzero
+--      generalized counts of successes and failures, alpha and beta.
+--      This is equivalent to
+--
+--              Exp $ log (alpha/(alpha + beta))
+--
+--      but computed more stably when beta <<< alpha:
+--
+--              log (alpha/(alpha + beta))
+--                = -log ((alpha + beta)/alpha)
+--                = -log (1 + beta/alpha)
+--                = -log1p (beta/alpha)
+--
+bernoulli_weight :: Double -> Double -> Log Double
+bernoulli_weight alpha beta = Exp $ -log1p (beta/alpha)
