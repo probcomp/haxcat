27bf20d1c11e398493dc3ba113619cf39ec97ea0
Alexey Radul <axch@mit.edu>
2015-09-27 13:15:11 -0400

    Draft sampling from the predictive distribution.
    - No constraints
    - No filtering of desired output columns
    - No multi-row joints

---
+++ Predictive.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Predictive where
+
+import Prelude hiding (mapM)
+
+import Control.Monad hiding (mapM)
+import qualified Data.Map as M
+import Data.Maybe (fromJust)
+import Data.Random.RVar
+import Data.Traversable
+
+import Models
+import Types
+
+view_cluster_sample :: View -> RVar ClusterID
+view_cluster_sample View{..} = sample_predictive view_counts view_crp
+
+column_sample :: ClusterID -> Column -> RVar Double
+column_sample c_id (Column hypers m) = sample_predictive stats hypers where
+  stats = fromJust $ M.lookup c_id m
+
+view_sample :: View -> RVar (M.Map ColID Double)
+view_sample v@View{..} = do
+  cluster_id <- view_cluster_sample v
+  mapM (column_sample cluster_id) view_columns
+
+cc_sample :: Crosscat -> RVar (M.Map ColID Double)
+cc_sample Crosscat{..} = liftM (M.foldl' M.union M.empty) per_view where
+    per_view = mapM view_sample cc_views
+                            
