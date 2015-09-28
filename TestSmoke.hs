0fce9b4380e240857859053757a9b8e87bc831b8
Alexey Radul <axch@mit.edu>
2015-09-27 20:45:03 -0400

    Bigger run.

--- TestSmoke.hs
+++ TestSmoke.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import qualified Data.Map
+import qualified Data.Map as M
 import qualified Data.Vector
 import Data.Random
 import Data.RVar (sampleRVar)
@@ -13,5 +13,18 @@ sampleIO = sampleRVar
 
 main :: IO ()
 main = do
-  sampleIO $ train (Data.Map.fromList [(ColID 0, Data.Vector.fromList [0])]) 2
+  cc <- sampleIO $ train (bogodata 1167 23) 1500
+  putStrLn $ show cc
   putStrLn "done"
+
+-- Up to the effects of input distribution on performance, satellites is
+-- train (bogodata 1167 23) 1500 in 30 minutes
+
+-- Admittedly, this particular data ends up all in one view and all in
+-- one cluster, so satellites would be slower (if the thing works)
+-- - A column sweep is O(table * num views)
+-- - A row sweep is O(table * avg_col num clusters)
+
+bogodata :: Int -> Int -> M.Map ColID (ColumnData Double)
+bogodata rows cols = M.fromList $ map column [0..cols-1] where
+    column col = (ColID col, Data.Vector.fromList $ map fromIntegral $ map (\x -> x + rows * col) [0..rows-1])
