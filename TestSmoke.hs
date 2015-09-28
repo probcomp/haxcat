81dbbec69d109880f183f1157c1e05837781722a
Alexey Radul <axch@mit.edu>
2015-09-27 21:36:34 -0400

    Try a somewhat more variegated dataset
    Synthesized row-wise-independently from a random view assignment.

--- TestSmoke.hs
+++ TestSmoke.hs
@@ -1,11 +1,14 @@
 module Main where
 
+import Control.Monad (replicateM)
 import qualified Data.Map as M
+import Data.Maybe (fromJust)
 import qualified Data.Vector
 import Data.Random
 import Data.RVar (sampleRVar)
 
 import Types
+import Predictive
 import Haxcat
 
 sampleIO :: RVar a -> IO a
@@ -13,7 +16,8 @@ sampleIO = sampleRVar
 
 main :: IO ()
 main = do
-  cc <- sampleIO $ train (bogodata 1167 23) 1500
+  ds <- sampleIO $ bogodata2 1167 23
+  cc <- sampleIO $ train ds 15
   putStrLn $ show cc
   putStrLn "done"
 
@@ -28,3 +32,14 @@ main = do
 bogodata :: Int -> Int -> M.Map ColID (ColumnData Double)
 bogodata rows cols = M.fromList $ map column [0..cols-1] where
     column col = (ColID col, Data.Vector.fromList $ map fromIntegral $ map (\x -> x + rows * col) [0..rows-1])
+
+bogodata2 :: Int -> Int -> RVar (M.Map ColID (ColumnData Double))
+bogodata2 rows cols = do
+  cc <- cc_initialize $ bogodata 0 cols
+  new_rows <- replicateM rows (cc_sample cc)
+  return $ reshape new_rows
+      where
+        reshape :: [Row] -> (M.Map ColID (ColumnData Double))
+        reshape rows = M.fromList $ map pick $ map ColID [0..cols-1]
+            where
+              pick col_id = (col_id, Data.Vector.fromList $ map (\(Row _ cell) -> fromJust $ cell col_id) rows)
