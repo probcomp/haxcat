a7b83dcce90a0e31584e0b782c365b2558237e08
Alexey Radul <axch@mit.edu>
2015-10-22 21:54:16 -0400

    Line breaks for code width.

--- Utils.hs
+++ Utils.hs
@@ -25,8 +25,9 @@ import Numeric.Log hiding (log1p, sum)
 import Numeric.SpecFunctions (log1p, logBeta, logGamma, logFactorial)
 
 flipweights :: [(a, Double)] -> RVar a
-flipweights weights = weightedCategorical [(exp (p - maxp), idx) | (idx, p) <- weights]
-    where maxp = maximum $ map snd weights
+flipweights weights = weightedCategorical weights'
+    where weights' = [(exp (p - maxp), idx) | (idx, p) <- weights]
+          maxp = maximum $ map snd weights
 
 logsumexp :: [Double] -> Double
 logsumexp xs = (log $ sum $ map exp adjusted) + max where
@@ -34,8 +35,9 @@ logsumexp xs = (log $ sum $ map exp adjusted) + max where
     max = maximum xs
 
 flipweights_ld :: [(a, Log Double)] -> RVar a
-flipweights_ld weights = weightedCategorical [(ln $ exp (p / maxp), idx) | (idx, p) <- weights]
-    where maxp = maximum $ map snd weights
+flipweights_ld weights = weightedCategorical weights'
+    where weights' = [(ln $ exp (p / maxp), idx) | (idx, p) <- weights]
+          maxp = maximum $ map snd weights
 
 choose :: Int -> Int -> Log Double
 choose n k = Exp $ logFactorial n - logFactorial k - logFactorial (n-k)
@@ -46,19 +48,24 @@ log_domain = Exp . log
 beta :: Double -> Double -> Log Double
 beta a b = Exp $ logBeta a b
 
-mapZipWith :: (Ord k) => (Maybe v1 -> Maybe v2 -> Maybe v3) -> M.Map k v1 -> M.Map k v2 -> M.Map k v3
-mapZipWith f m1 m2 = M.map fromJust $ M.filter isJust $ M.map (uncurry f) $ mapZipWithHoles m1 m2
+mapZipWith :: (Ord k) => (Maybe v1 -> Maybe v2 -> Maybe v3)
+           -> M.Map k v1 -> M.Map k v2 -> M.Map k v3
+mapZipWith f m1 m2 = M.map fromJust
+                   $ M.filter isJust
+                   $ M.map (uncurry f) $ mapZipWithHoles m1 m2
 
-mapZipWithHoles :: (Ord k) => M.Map k v1 -> M.Map k v2 -> M.Map k (Maybe v1, Maybe v2)
-mapZipWithHoles m1 m2 = M.fromAscList $ go (M.toAscList m1) (M.toAscList m2) where
-    go [] [] = []
-    go [] ((k,v):rest) = (k, (Nothing, Just v)):go [] rest
-    go ((k,v):rest) [] = (k, (Just v, Nothing)):go rest []
-    go vs1@((k1, v1):r1) vs2@((k2, v2):r2) =
-        case k1 `compare` k2 of
-          LT -> (k1, (Just v1, Nothing)):go  r1 vs2
-          GT -> (k2, (Nothing, Just v2)):go vs1  r2
-          EQ -> (k1, (Just v1, Just v2)):go  r1  r2
+mapZipWithHoles :: (Ord k) => M.Map k v1 -> M.Map k v2
+                -> M.Map k (Maybe v1, Maybe v2)
+mapZipWithHoles m1 m2 = M.fromAscList $ go (M.toAscList m1) (M.toAscList m2)
+    where
+      go [] [] = []
+      go [] ((k,v):rest) = (k, (Nothing, Just v)):go [] rest
+      go ((k,v):rest) [] = (k, (Just v, Nothing)):go rest []
+      go vs1@((k1, v1):r1) vs2@((k2, v2):r2) =
+          case k1 `compare` k2 of
+            LT -> (k1, (Just v1, Nothing)):go  r1 vs2
+            GT -> (k2, (Nothing, Just v2)):go vs1  r2
+            EQ -> (k1, (Just v1, Just v2)):go  r1  r2
 
 gamma :: Double -> Log Double
 gamma x = Exp $ logGamma x
