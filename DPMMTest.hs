b0dea26bed072cde529da116426e4bd3fb484663
Alexey Radul <axch@mit.edu>
2015-11-22 16:55:55 -0500

    Rewrite DPMMTest in terms of the distribution abstractions in Models.
    This caused a minor difference in the order of application of some
    numerics, slightly twiddling the test answers.

--- DPMMTest.hs
+++ DPMMTest.hs
@@ -14,70 +14,72 @@
 --   See the License for the specific language governing permissions and
 --   limitations under the License.
 
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+
 module DPMMTest where
 
 import Data.RVar (sampleRVar)
-import Data.Random
+import Data.Random hiding (sample, pdf, PDF)
 import Data.Random.Distribution.Bernoulli
 import Control.Monad
+import qualified Numeric.Log as Log
 
+import Models
 import Utils
 import DPMM
 
-type LogDensity a = a -> Double
-type Assessable a = (RVar a, LogDensity a)
-type TailAssessable a = RVar (Assessable a)
+data TailAssessable a = forall m. (Model m a) => TailAssessable (RVar m)
 
-mixture_density :: [LogDensity a] -> LogDensity a
-mixture_density ds x = logsumexp (map ($ x) ds) - log (fromIntegral $ length ds)
+mixture_density :: [PDF a] -> PDF a
+mixture_density ds x = Log.sum (map ($ x) ds) / log_domain (fromIntegral $ length ds)
 
--- This is a mixture of two Gaussians (-3/1 and 3/1).
-two_modes :: Assessable Double
-two_modes = (sample, logd) where
-    sample = do
-      pos_mode <- bernoulli (0.5 :: Double)
-      if pos_mode then
-          normal 3 1
-      else
-          normal (-3) 1
-    logd x = logsumexp [logPdf (Normal 3 1) x, logPdf (Normal (-3) 1) x] - log 2
+two_modes :: CatMixture Double
+two_modes = CatMixture [ (0.5, CatComponent $ DistModel $ Normal 3 1)
+                       , (0.5, CatComponent $ DistModel $ Normal (-3) 1)]
 
 two_modes_ta :: TailAssessable Double
-two_modes_ta = do
+two_modes_ta = TailAssessable (do
   pos_mode <- bernoulli (0.5 :: Double)
   if pos_mode then
       return $ gaussian 3 1
   else
-      return $ gaussian (-3) 1
+      return $ gaussian (-3) 1)
 
-gaussian :: Double -> Double -> Assessable Double
-gaussian mu sig = (normal mu sig, logPdf (Normal mu sig))
+gaussian :: Double -> Double -> DistModel (Normal Double)
+gaussian mu sig = DistModel $ (Normal mu sig)
 
-estimate_KL :: Assessable a -> LogDensity a -> Int -> RVar Double
+estimate_KL :: (Model m a) => m -> PDF a -> Int -> RVar Double
 estimate_KL from to sample_ct = do
-  input <- replicateM sample_ct (fst from)
+  input <- replicateM sample_ct (sample from)
   return $ (sum $ map term input) / (fromIntegral $ length input)
-    where term x = (snd from) x - to x
+    where term x = Log.ln $ (pdf from x) / (to x)
 
-approximately_assess :: Int -> TailAssessable a -> RVar (LogDensity a)
-approximately_assess ct dist = do
-  assessors <- liftM (map snd) $ replicateM ct dist
+approximately_assess :: Int -> TailAssessable a -> RVar (PDF a)
+approximately_assess ct (TailAssessable dist) = do
+  assessors <- liftM (map pdf) $ replicateM ct dist
   return $ mixture_density assessors
 
-estimate_KL_ta :: Assessable a -> TailAssessable a -> Int -> Int -> RVar Double
+estimate_KL_ta :: (Model m a) => m -> TailAssessable a -> Int -> Int -> RVar Double
 estimate_KL_ta from to latents_ct sample_ct = do
   density <- approximately_assess latents_ct to
   estimate_KL from density sample_ct
 
-dpmm_dist :: DPMM -> Assessable Double
-dpmm_dist dpmm = (return $ error "What?", predictive_logdensity dpmm)
+-- TODO Is this really the instance I want?  This is the predictive
+-- distribution; there may be a good class for the DPMM that captures
+-- more of its structure.
+instance Model DPMM Double where
+    sample _ = return $ error "What?"
+    pdf m = Log.Exp . predictive_logdensity m
 
 trained_dpmm :: [Double] -> Int -> TailAssessable Double
-trained_dpmm input iters = liftM dpmm_dist $ train_dpmm input iters
+trained_dpmm input iters = TailAssessable $ train_dpmm input iters
 
-measure_dpmm_kl :: Assessable Double -> Int -> Int -> Int -> Int -> RVar Double
+measure_dpmm_kl :: (Model m Double) => m -> Int -> Int -> Int -> Int -> RVar Double
 measure_dpmm_kl data_gen train_data_ct iter_ct chain_ct test_ct = do
-  input <- replicateM train_data_ct (fst data_gen)
+  input <- replicateM train_data_ct (sample data_gen)
   estimate_KL_ta data_gen (trained_dpmm input iter_ct) chain_ct test_ct
 
 sampleIO :: RVar a -> IO a
