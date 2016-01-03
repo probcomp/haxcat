ac13167d68d32e67d0f71211a1988081de0d89f0
Alexey Radul <axch@mit.edu>
2016-01-03 17:47:26 -0500

    Parameterize the test plot; add a self-test plot for Distribution.ChiSquare.

--- ChiSquareTest.hs
+++ ChiSquareTest.hs
@@ -4,7 +4,8 @@ import Control.Monad
 import Control.Monad.Trans
 import Data.Random
 import Data.Random.Distribution.Categorical
-import Data.RVar (sampleRVarT)
+import Data.Random.Distribution.ChiSquare
+import Data.RVar (sampleRVar, sampleRVarT)
 import Graphics.Rendering.Chart.Gtk
 
 import ChiSquare
@@ -13,17 +14,23 @@ import Plotting.PPPlot
 test_weights :: [(Double, Double)]
 test_weights = [(0.25, 1), (0.25, 2), (0.25, 3), (0.25, 4)]
 
-the_p_value :: (Monad m) => RVarT m Double
-the_p_value = do
-  data1 <- replicateM 250 $ categoricalT test_weights
-  data2 <- replicateM 250 $ categoricalT test_weights
+the_p_value :: (Monad m) => Int -> RVarT m Double
+the_p_value samples = do
+  data1 <- replicateM samples $ categoricalT test_weights
+  data2 <- replicateM samples $ categoricalT test_weights
   return $ chi_square_p data1 data2
 
-the_plot :: RVarT IO ()
-the_plot = do
-  ps <- replicateM 500 the_p_value
+the_plot :: Int -> Int -> RVarT IO ()
+the_plot samples replications = do
+  ps <- replicateM replications $ the_p_value samples
   lift $ toWindow 300 300 $ p_p_plot (Continuous "uniform" id)
            (Empirical "p-values" ps)
 
-do_the_plot :: IO ()
-do_the_plot = sampleRVarT the_plot
+do_the_plot :: Int -> Int -> IO ()
+do_the_plot samples replications = sampleRVarT $ the_plot samples replications
+
+chisq_self_test_plot :: Int -> IO ()
+chisq_self_test_plot samples = do
+  xs <- sampleRVar $ replicateM samples $ chiSquare 3
+  toWindow 300 300 $ p_p_plot (Continuous "cdf" $ cdf $ ChiSquare 3)
+               (Empirical "samples" (xs :: [Double]))
