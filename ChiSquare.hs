8e70bb561e952cda5edf3be161338783eacd0c5d
Alexey Radul <axch@mit.edu>
2016-01-03 17:48:23 -0500

    Reading, thinking.

--- ChiSquare.hs
+++ ChiSquare.hs
@@ -28,6 +28,45 @@ import qualified Utils as U
 -- Two sample Chi^2 test from
 -- http://www.itl.nist.gov/div898/software/dataplot/refman1/auxillar/chi2samp.htm
 
+-- The implication from [1] is that two-sample Chi^2 test of sameness
+-- is the same as the one-sample Chi^2 test of fit of the disjoint
+-- union of the samples to the independent product of the total
+-- observed frequencies in the whole data set with the correctly
+-- weighted binary distribution on labels.
+--
+-- In symbols: Given two samples xs :: [a] and ys :: [a], consider
+-- the disjoint union
+--   union :: [a] -> [b] -> [Either a b]
+--   union xs ys = (map Left xs) ++ (map Right ys)
+-- If xs and ys are drawn from the same underlying distribution,
+-- the following two distributions of type RVar (Either a a) are
+-- the same:
+--
+--   dependently :: RVar a -> RVar a -> Double -> RVar (Either a a)
+--   dependently x y p = do
+--     label <- bernoulli p
+--     if label then do
+--       value <- x
+--       return $ Left value
+--     else do
+--       value <- y
+--       return $ Right value
+--
+--   independently :: RVar a -> Double -> RVar (Either a a)
+--   independently x p = do
+--     label <- categorical $ [(p, Left), (1-p, Right)]
+--     value <- x
+--     return $ label value
+--
+-- We treat the disjoint union of our observations as n+m samples from
+-- `dependently`, and compare to what we would expect from
+-- `independently`, where `p` and the frequencies in `x` are derived
+-- from the data (to maximize the likelihood of the observations).
+--
+-- The analysis suggests that the degrees of freedom are the number of
+-- bins - 1, which is the total number of quantities observed minus
+-- the number of parameters to fit for the `independently` hypothesis.
+
 -- Ord for my convenience only.  a is the bin type.
 -- Returns the statistic and the measured number of degrees of freedom.
 chi_square_stat :: (Eq a, Ord a) => [a] -> [a] -> (Double, Int)
@@ -54,3 +93,8 @@ pair_with_defaults def1 def2 =
 chi_square_p :: (Eq a, Ord a) => [a] -> [a] -> Double
 chi_square_p data1 data2 = 1 - cdf (ChiSquare $ fromIntegral dof) stat where
     (stat, dof) = chi_square_stat data1 data2
+
+-- References
+
+-- [1] Jesse Hoey, "The Two-Way Likelihood Ratio (G) Test", June 2012,
+-- http://arxiv.org/abs/1206.4881
